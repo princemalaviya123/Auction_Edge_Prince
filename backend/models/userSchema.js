9b8e3da274c925882be31992fb23f4810275862a
princemalaviya123 <princemalaviya16@gmail.com>
2025-03-08 00:02:02 +0530

    Fixed bcrypt deployment issue again

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     userName: {
